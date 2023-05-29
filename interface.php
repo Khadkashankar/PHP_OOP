f3ef6d38d95334be580ebf0c4182425d4d351b3a
Shankar Khadka <107821088+Khadkashankar@users.noreply.github.com>
2023-05-29 12:16:10 +0545

    Add files via upload

--- interface.php
+++ interface.php
@@ -1,5 +1,16 @@
 <?php
-
+//interface class
+interface Father{
+    const a = 10;       //interface class contains constant.
+    public function displayValue();     //interface class contains abstract methods which have only declarations.
+}
+class Son implements Father{
+    function displayValue(){
+        echo self::a;
+    }
+}
+$object = new Son;
+$object->displayValue();
 
 
 ?>
